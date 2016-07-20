4d1721bd37acbe7b08d8d1abc84090accc6e6e55
Ing. Miroslav Biňas PhD <miroslav.binas@tuke.sk>
2016-07-20 10:11:55 +0200

    rozsirenie sablony
    - pridanie analytickeho listu do prace
    - pridanie prikazu \title{}{} na zadefinovanie slovenskeho a anglickeho
      prekladu prace
    - pridana experimentalna podpora pre luatex (kvoli naformatovaniu nazvu
      prace)
    - niekolko oprav a rozsirenie informacii o latex-u a pisani prace

---
+++ thesis/functions.lua
@@ -0,0 +1,11 @@
+function rmLineEnds(text)
+    print('>>>>>>>>>>>>>>>> here we go')
+    local s, _ = string.gsub(text, "a", "X")
+    return s
+end
+
+function rawText(text)
+    text = string.gsub(text, "\\\\", "")
+    print(text)
+    tex.print(text)
+end
