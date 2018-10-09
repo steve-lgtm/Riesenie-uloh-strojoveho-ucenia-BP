9fb544de4d9fa4a210f0cf8b2985a49225f5c144
Ing. Miroslav Biňas PhD <miroslav.binas@tuke.sk>
2018-10-09 10:21:44 +0200

    install.sh a sablona
    * install.sh
      * zobrazi aj umiestnenie priecinku v systeme, kde sa spusta
      * opravene umiestnenie interpretera (prenositelnost)
    
    * kithesis.cls
      * zruseny analyticky list a nahradeny len abstraktom a klucovymi slovami

--- dist/install.sh
+++ dist/install.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script installs the packages, classes and styles in local TeX directory
 # in OS Linux with TeX Live distribution.
 
 TEXMFHOME=$(kpsewhich -var-value=TEXMFHOME)
+echo "TEXMFHOME is located in ${TEXMFHOME}."
+echo "Making directories and copying files..."
+
 mkdir -p $TEXMFHOME/tex/latex/tuke/
 cp *.{sty,cls} $TEXMFHOME/tex/latex/tuke/
 
+echo "Done"
